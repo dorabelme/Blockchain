a7871f971b0f47bae458b7017f0899a2fea15cf6
Dora Belme <contact.dorabelme@gmail.com>
2020-01-15 15:13:33 -0500

    fixing miner file

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -3,6 +3,9 @@ import requests
 
 import sys
 import json
+import time
+
+DIFFICULTY = 6
 
 
 def proof_of_work(block):
@@ -13,7 +16,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +34,29 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:DIFFICULTY] == '0' * DIFFICULTY
+
+
+# def new_block(proof, previous_block):
+
+#     block = {
+#         'index': previous_block['index'] + 1,
+#         'timestamp': time.time(),
+#         'transactions': [],
+#         'proof': proof,
+#         'previous_hash': previous_block['previoius_hash']
+#     }
+
+#     return block
+
+
+# def hash(block):
+#     string_object = json.dumps(block, sort_keys=True).encode()
+#     raw_hash = hashlib.sha256(string_object)
+#     hex_hash = raw_hash.hexdigest()
+#     return hex_hash
 
 
 if __name__ == '__main__':
@@ -36,6 +65,8 @@ if __name__ == '__main__':
         node = sys.argv[1]
     else:
         node = "http://localhost:5000"
+
+    coins_mined = 0
 
     # Load ID
     f = open("my_id.txt", "r")
@@ -57,6 +88,10 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        block = data['last_block']
+        print('Getting new proof.')
+        new_proof = proof_of_work(block)
+        print('Posting to server.')
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +102,6 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data['message'] == 'New Block Forged':
+            coins_mined += 1
+            print('Received 1 coin, total coins: ', coins_mined)
