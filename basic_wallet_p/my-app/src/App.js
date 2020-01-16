02e402143da5710b8286c751781aea7e0e545cd2
Dora Belme <contact.dorabelme@gmail.com>
2020-01-16 18:07:16 -0500

    MVP day 2

--- basic_wallet_p/my-app/src/App.js
+++ basic_wallet_p/my-app/src/App.js
@@ -1,24 +1,104 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Display from './components/Display';
+import Form from './components/Form';
+import axios from "axios";
 import './App.css';
 
+const initialState = {
+  chain: [],
+  title: "Whole Chain",
+  coins: null,
+  filteredChain: [],
+  id: null
+}
+
 function App() {
+
+  const [state, setState] = useState(initialState)
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/chain')
+      .then(({ data }) => {
+        console.log(data)
+        setState({ ...state, chain: data.chain });
+        console.log("STATE", state);
+      })
+      .catch(err => console.error(err))
+
+  }, [])
+
+  const filterChain = (id) => {
+    console.log("CHAIN: ", state.chain, "\n id: ", id)
+    const filteredChain = state.chain.filter(block => {
+      console.log("block: ", block)
+      if (!block.transactions.length) {
+        return false
+      }
+      else {
+        return block.transactions[0].recipient === id
+      }
+    })
+    console.log("FILTER", filteredChain, id)
+    if (!filteredChain.length) {
+      setState({
+        ...state,
+        warning: "No user by that id was found",
+        title: "Whole Chain",
+        filteredChain: [],
+        coins: null
+      })
+      setTimeout(() => {
+        setState({ ...state, warning: '' })
+      }, 3000);
+    }
+    else {
+      setState(
+        {
+          ...state,
+          id,
+          title: `${id}'s Transactions`,
+          filteredChain,
+          warning: null
+        }
+      )
+      setTimeout(() => {
+        getCoins(id)
+      }, 500);
+    }
+  }
+
+  const getCoins = (id) => {
+    let totalCoins = 0;
+    state.filteredChain.forEach(block => {
+      console.log("COINS FOREACH: ", block, id)
+      if (block.transactions[0].recipient === id) {
+        return totalCoins = block.transactions[0].amount + totalCoins
+      }
+      if (block.transactions[0].sender === id) {
+        return totalCoins -= block.transactions[0].amount
+      }
+    })
+    setState({ ...state, coins: totalCoins })
+  }
+
+  console.log("coinie", state.coins)
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        filterChain={filterChain}
+        getCoins={getCoins}
+      />
+      <h2>{state.warning}</h2>
+      <Display
+        coins={state.coins}
+        chain={state.filteredChain.length ? state.filteredChain : state.chain}
+        title={state.title}
+        length={state.chain.length}
+      />
     </div>
   );
 }
